1c637f8afe2105fd8eba93e24911c503eb7e0044
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-03-06 18:42:35 -0500

    finish server

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,17 +1,85 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
-const { SECRET_KEY } = require('../../config')
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong crendetials";
+        throw new UserInputError("Wrong crendetials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+      const emailVal = await User.findOne({ email });
+      if (emailVal) {
+        throw new UserInputError("Email is taken", {
+          errors: {
+            email: "This email is taken",
+          },
+        });
+      }
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
@@ -20,12 +88,16 @@ module.exports = {
         password,
         createdAt: new Date().toISOString(),
       });
-      const res = await newUser.save()
-      const token = jwt.sign({
-          id: res.id,
-          email: res.email,
-          username: res.username
-      }) 
+
+      const res = await newUser.save();
+
+      const token = generateToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,4 +1,4 @@
-const gql = require("graphql-tag");
+const { gql } = require('apollo-server');
 
 module.exports = gql`
   type Post {
@@ -6,25 +6,49 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
   }
-  type User{
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
+  }
+  type User {
     id: ID!
     email: String!
     token: String!
     username: String!
-    createdAt:String!
+    createdAt: String!
   }
-  input RegisterInput{
+  input RegisterInput {
     username: String!
     password: String!
     confirmPassword: String!
     email: String!
   }
-
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
-  type Mutation{
+  type Mutation {
     register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+  type Subscription {
+    newPost: Post!
   }
 `;
